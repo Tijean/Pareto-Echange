def373cd53bc2ff602db3a13b41f8e1a543cb081
phildragon <techno-phil91@hotmail.com>
2016-04-03 15:59:43 -0400

    update-boucle

--- src/pareto/echange/ParetoEchange.java
+++ src/pareto/echange/ParetoEchange.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pareto.echange;
+package pareto;
 
 /**
  *
@@ -12,8 +12,44 @@ package pareto.echange;
 public class ParetoEchange {
 
     /**
+     * vidu
+     *
      * @param args the command line arguments
      */
+    public static void bruteforce(Individus individu1, Individus individu2) {
+
+        int total[] = new int[2];
+        total[0] = individu1.production[0] + individu2.production[0];
+        total[1] = individu1.production[1] + individu2.production[1];
+        double max = 0.0;
+        int maxi = 0;
+        int maxj = 0;
+        for (int i = 0; i <= total[0]; i++) {
+            for (int j = 0; j <= total[0]; j++) {
+                int tab_cons[] = new int[2];
+                int tab_cons2[] = new int[2];
+                tab_cons[0] = i;
+                tab_cons[1] = j;
+                tab_cons2[0] = total[0] - i;
+                tab_cons2[1] = total[1] - i;
+                double temp = individu1.utilite(tab_cons);
+                double temp2 = individu2.utilite(tab_cons);
+                if (individu1.u_valide(temp) && individu2.u_valide(temp2)) {
+                    if (temp + temp2 > max) {
+                        max = temp + temp2;
+                        maxi = i;
+                        maxj = j;
+                    }
+                }
+
+            }
+        }
+        System.out.println(maxi);
+        System.out.println(maxj);
+        System.out.println(max);
+    }
+    
+
     public static void main(String[] args) {
         System.out.println(java.lang.Math.pow(10, 0.5));
         int p1[] = {10, 0};
@@ -26,11 +62,13 @@ public class ParetoEchange {
          double u2 = ind2.utilite(p2);
          System.out.println("Individus 2 = " + u2);
          */
+        
         System.out.println("Individus1 " + ind1);
         System.out.println("Individus2 " + ind2);
         ind1.consommation[0] = 5;
         ind1.consommation[1] = 5;
-        System.out.println("Ind1 final = " +ind1);
+        System.out.println("Ind1 final = " + ind1);
+        bruteforce(ind1, ind2);
     }
-    
+
 }
